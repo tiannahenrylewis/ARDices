23d8fbcdc043fe494474073a22cd164fd9751bc7
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2018-09-28 11:16:26 -0400

    Dice Implementation

--- ARDices/ViewController.swift
+++ ARDices/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
-        
+
+//
 //        //Create a cube object
 //        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
 //
