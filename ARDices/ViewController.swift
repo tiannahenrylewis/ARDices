c1b762160941431a1b92f901b133627ded91767b
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2018-10-01 12:46:40 -0400

    Placing Dice in 3D Using Touch

--- ARDices/ViewController.swift
+++ ARDices/ViewController.swift
@@ -45,17 +45,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         //Add Light
         sceneView.autoenablesDefaultLighting = true
         
-//        // Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//
-//        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-//
-//        sceneView.scene.rootNode.addChildNode(diceNode)
-//        }
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -85,10 +74,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
-            if !results.isEmpty {
-                print("touched the plane")
-            } else {
-                print("touched somewhere else")
+            if let hitResult = results.first {
+                
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+        
+                diceNode.position = SCNVector3(
+                    x: hitResult.worldTransform.columns.3.x,
+                    y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                    z: hitResult.worldTransform.columns.3.z
+                )
+        
+                sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                }
             }
         }
     }
